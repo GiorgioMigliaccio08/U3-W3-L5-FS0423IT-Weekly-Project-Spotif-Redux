b96f33395618fc9329af3a2dbc488ad08c7a0adc
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-10 12:33:35 +0100

    Add Home.jsx and Album.jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SideBar from "./components/Sidebar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <SideBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
